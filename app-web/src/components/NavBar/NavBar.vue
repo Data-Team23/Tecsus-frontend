fc18e9b3194e939a77fbb7ffe3199256c242964a
Ariane-Sousa <ariane.crisousa@gmail.com>
2024-03-29 21:08:31 -0300

    style: alterando logo

--- app-web/src/components/NavBar/NavBar.vue
+++ app-web/src/components/NavBar/NavBar.vue
@@ -1,6 +1,9 @@
-"<template>
+<template>
 <div>
     <div class="navbar-superior">
+        <div>
+            <img src="@/assets/logo.png" alt="Logo" class="logo">
+        </div>
     </div>
     <div :class="['navbar-lateral', { 'dobrada': dobrada }]">
     <div class="icon">
