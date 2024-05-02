5d73cbe0b69c28a1c0fb132febd966ee11f6da15
Silas Prado <silas.prado98@gmail.com>
2024-05-01 21:44:59 -0300

    feat: integrando requisição de alerta com backend

--- app-web/src/views/WaterAlerts.vue
+++ app-web/src/views/WaterAlerts.vue
@@ -19,69 +19,27 @@
 import { ref } from 'vue';
 import TableComponent from '@/components/Table/TableComponent.vue';
 import '@/assets/styles/alerts.css';
-import dataMocked from '@/assets/mockedDatas/waterAlerts.json'
+import axios from 'axios';
 
 import { useToast } from 'vue-toastification';
 const toast = useToast();
 
-const apiUrl = ''
+const apiUrl = 'http://localhost:8000/api'
 
-const data = ref([
-    {
-        "USUÁRIO": "User 1",
-        "EMAIL": "user1@gmail.com",
-        "ID DO ALERTA": "1837998",
-        "ALERTA": "10",
-        "CONSUMO": "6",
-        "MÊS": "01/01/2024",
-        "MÈDIA": "5",
-    },
-    {
-        "USUÁRIO": "User 2",
-        "EMAIL": "user2@gmail.com",
-        "ID DO ALERTA": "3347998",
-        "ALERTA": "16",
-        "CONSUMO": "10",
-        "MÊS": "01/03/2024",
-        "MÈDIA": "8",
-    },
-    {
-        "USUÁRIO": "User 1",
-        "EMAIL": "user1@gmail.com",
-        "ID DO ALERTA": "2237998",
-        "ALERTA": "11",
-        "CONSUMO": "7",
-        "MÊS": "01/02/2024",
-        "MÈDIA": "5",
-    },
-])
+const data = ref([])
 
-let tableObject = {
-    id: "374682736",
-    name: "Conta de Luz",
-    value: "50.000",
-    year: "2023",
-    code: "451",
-    unity: "Unidade 1",
-};
+let tableObject = {};
 
-// fetch(apiUrl, {  
-//     method: 'GET',
-// })
-// .then(response => {
-//     if (response.ok) {
-//         data.value = response.value
-//     } else {
-//         toast.error('Erro carregar alertas.', {
-//           position: 'bottom-center'
-//         });
-//     }
-// })
-// .catch(() => {
-//     toast.error('Erro ao carregar alertas.', {
-//         position: 'bottom-center'
-//     });
-// });
+axios.get(`${apiUrl}/alerta/listar`)
+  .then(response => {
+    console.log(response.data);
+    data.value = response.data
+  })
+  .catch(error => {
+    console.error('Erro ao carregar alertas:', error);
+  });
+
+
 
 
 </script>
