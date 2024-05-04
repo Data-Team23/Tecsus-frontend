69b09340ef743f38d66e9c32f14fa52e5ac50e0e
Silas Prado <silas.prado98@gmail.com>
2024-05-04 00:14:09 -0300

    fix: correção na url para api de alertas

--- app-web/src/views/WaterAlerts.vue
+++ app-web/src/views/WaterAlerts.vue
@@ -20,8 +20,8 @@ import { ref } from 'vue';
 import TableComponent from '@/components/Table/TableComponent.vue';
 import '@/assets/styles/alerts.css';
 import axios from 'axios';
-
 import { useToast } from 'vue-toastification';
+
 const toast = useToast();
 
 const apiUrl = 'http://localhost:8000/api'
@@ -30,7 +30,7 @@ const data = ref([])
 
 let tableObject = {};
 
-axios.get(`${apiUrl}/alerta/listar`)
+axios.get(`${apiUrl}/agua/alertas_agua`)
   .then(response => {
     console.log(response.data);
     data.value = response.data
